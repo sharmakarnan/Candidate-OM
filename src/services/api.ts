1fcd3b552872efb1ecb7d2130cdc1c53da900c95
sharmakarnan <132341863+sharmakarnan@users.noreply.github.com>
2025-11-20 12:41:02 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://candidate-be-60058709024.development.catalystserverless.in/server/candidate_be_function/", 
+  baseURL: "https://candidate-be-60058709024.development.catalystserverless.in/server/candidate_be_function", 
 });
 
 export default api;
